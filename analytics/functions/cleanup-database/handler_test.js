b24bb6b4e34ad569483541fd0b3713c8296601c3
Rocco Zanni <rocco.zanni@spreaker.com>
2023-10-27 15:38:45 +0200

    Data retention for unsuccessful measurements

--- analytics/functions/cleanup-database/handler_test.js
+++ analytics/functions/cleanup-database/handler_test.js
@@ -41,4 +41,35 @@ describe('analytics - cleanupDatabase', () => {
         assert.deepStrictEqual(rows, [measurements[0]]);
     });
 
+    it('should delete old unavailables', async () => {
+
+        const unavailables = [
+            {
+                id: randomUUID(), timestamp: formatISO(new Date()), endpoint: "service",
+                type: "enclosure", region: "antartica-1", details: '{}'
+            },
+            {
+                id: randomUUID(), timestamp: formatISO(subDays(new Date(), 32)), endpoint: "service",
+                type: "enclosure", region: "antartica-1", details: '{}'
+            },
+        ];
+
+        for (const unavailable of unavailables) {
+            await db.query(`
+            INSERT INTO unavailables (id, timestamp, endpoint, type, region, details)
+            VALUES ($1, $2, $3, $4, $5, $6)
+        `, [
+                unavailable.id, unavailable.timestamp, unavailable.endpoint,
+                unavailable.type, unavailable.region, unavailable.details
+            ]);
+        }
+
+        await cleanupDatabase();
+
+        const rows = (await query("SELECT * FROM unavailables")).rows;
+
+        assert.equal(rows.length, 1);
+        assert.deepStrictEqual(rows, [unavailables[0]]);
+    });
+
 });
