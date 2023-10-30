04682ba580e36741b48349d8947325e93190e21e
Emanuele Rampichini <emanuele.rampichini@gmail.com>
2023-10-30 14:02:41 +0100

    Tabs to spaces

--- website/src/lib/Api.ts
+++ website/src/lib/Api.ts
@@ -2,64 +2,64 @@ import { Detail, Instant, Issue, ServiceInstant } from '../types'
 import { API_BASE_URL } from './Constants'
 
 export async function fetchInstantData(region: string): Promise<Instant[]> {
-	return await fetch(`${API_BASE_URL}/api/instant-${region}.json`)
-		.then((response) => response.json() as Promise<Instant[]>)
-		.then((data) => {
-			return data
-		})
-		.catch(() => {
-			return []
-		})
+  return await fetch(`${API_BASE_URL}/api/instant-${region}.json`)
+    .then((response) => response.json() as Promise<Instant[]>)
+    .then((data) => {
+      return data
+    })
+    .catch(() => {
+      return []
+    })
 }
 
 export async function fetchServiceInstantsData(
-	endpoint: string,
-	region: string
+  endpoint: string,
+  region: string
 ): Promise<ServiceInstant[]> {
-	return await fetch(`${API_BASE_URL}/api/instant-${endpoint}-${region}.json`)
-		.then((response) => response.json() as Promise<ServiceInstant[]>)
-		.then((data) => {
-			return data
-		})
-		.catch(() => {
-			return []
-		})
+  return await fetch(`${API_BASE_URL}/api/instant-${endpoint}-${region}.json`)
+    .then((response) => response.json() as Promise<ServiceInstant[]>)
+    .then((data) => {
+      return data
+    })
+    .catch(() => {
+      return []
+    })
 }
 
 export async function fetchDetailedData(endpoint: string, region: string): Promise<Detail[]> {
-	return await fetch(`${API_BASE_URL}/api/detailed-${endpoint}-${region}.json`)
-		.then((response) => response.json() as Promise<Detail[]>)
-		.then((data) => {
-			return data
-		})
-		.catch(() => {
-			return []
-		})
+  return await fetch(`${API_BASE_URL}/api/detailed-${endpoint}-${region}.json`)
+    .then((response) => response.json() as Promise<Detail[]>)
+    .then((data) => {
+      return data
+    })
+    .catch(() => {
+      return []
+    })
 }
 
 export async function fetchRecentIssues(endpoint: string, region: string): Promise<Issue[]> {
-	return await fetch(`${API_BASE_URL}/api/recent-issues-${endpoint}-${region}.json`)
-		.then((response) => response.json() as Promise<Issue[]>)
-		.then((data) => {
-			return data
-		})
-		.catch(() => {
-			return []
-		})
+  return await fetch(`${API_BASE_URL}/api/recent-issues-${endpoint}-${region}.json`)
+    .then((response) => response.json() as Promise<Issue[]>)
+    .then((data) => {
+      return data
+    })
+    .catch(() => {
+      return []
+    })
 }
 
 export async function fetchDetailedAndServiceInstantsAndRecentIssuesData(
-	endpoint: string,
-	region: string
+  endpoint: string,
+  region: string
 ): Promise<{
-	detailed: Detail[]
-	instants: ServiceInstant[]
-	issues: Issue[]
+  detailed: Detail[]
+  instants: ServiceInstant[]
+  issues: Issue[]
 }> {
-	const [detailed, instants, issues] = await Promise.all([
-		fetchDetailedData(endpoint, region),
-		fetchServiceInstantsData(endpoint, region),
-		fetchRecentIssues(endpoint, region)
-	])
-	return { detailed, instants, issues }
+  const [detailed, instants, issues] = await Promise.all([
+    fetchDetailedData(endpoint, region),
+    fetchServiceInstantsData(endpoint, region),
+    fetchRecentIssues(endpoint, region)
+  ])
+  return { detailed, instants, issues }
 }
